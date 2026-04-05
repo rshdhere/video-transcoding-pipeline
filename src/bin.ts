2cb54263efbabc1248d82210127c39be32943295
raashed <mohammedraashed10@gmail.com>
2026-04-05 23:26:15 +0530

    fix(dotenv): added a bin file to run our server

--- src/bin.ts
+++ src/bin.ts
@@ -1,1 +1,7 @@
-console.log('hello there')
+import "dotenv/config";
+import { app } from "./index.js";
+import { PORT } from "./config/config.js";
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
