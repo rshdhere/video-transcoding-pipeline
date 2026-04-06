3d77ac4b95a32aeb8a56181210797b5e384e3247
raashed <mohammedraashed10@gmail.com>
2026-04-06 14:08:40 +0530

    feat(sqs): added email verification under a queue-job using /aws-sqs

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { dbClient } from "../drizzle/src/index.js";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "../drizzle/src/database/schema.js";
+import { enqueueEmailJob } from "@/queue/enqueue.js";
 
 export const auth = betterAuth({
   database: drizzleAdapter((dbClient), {
@@ -9,6 +10,17 @@ export const auth = betterAuth({
     provider: "pg"
   }),
   emailAndPassword: {
-    enabled: true
+    enabled: true,
+    requireEmailVerification: true
+  },
+
+  emailVerification: {
+    async sendVerificationEmail({ url, user }) {
+      await enqueueEmailJob({
+        type: "VERIFY_EMAIL",
+        userEmail: user.email,
+        verificationUrl: url
+      })
+    }
   }
 })
