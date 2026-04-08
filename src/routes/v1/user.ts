4cb7802d56eb6089eb676fd7215b3cea3b2e44f7
raashed <mohammedraashed10@gmail.com>
2026-04-08 15:27:24 +0530

    refactor(user): added proper error-handling for signup & login tRPC-procedures

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,31 +1,56 @@
 import { auth } from "@/auth/auth.js";
+import { TRPCError } from "@trpc/server";
 import { authSchema } from "@/types/index.js";
 import { publicProcedure, router } from "@/trpc.js";
 
 export const userRouter = router({
   signup: publicProcedure.input(authSchema.input).mutation(async ({ input }) => {
 
-    const response = await auth.api.signUpEmail({
-      body: {
-        name: input.name,
-        email: input.email,
-        password: input.password,
-      }
-    })
+    try {
+      const response = await auth.api.signUpEmail({
+        body: {
+          name: input.name,
+          email: input.email,
+          password: input.password
+        }
+      })
 
-    return response
+      return response
+
+    } catch (error: any) {
+
+      console.error("[error]:", JSON.stringify(error, null, 2))
+
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: error.message || "unknown error"
+      })
+    }
 
   }),
 
   login: publicProcedure.input(authSchema.input).mutation((async ({ input }) => {
-    const response = await auth.api.signInEmail({
-      body: {
-        email: input.email,
-        password: input.password
-      }
-    })
 
-    return response
+    try {
+      const response = await auth.api.signInEmail({
+        body: {
+          email: input.email,
+          password: input.password
+        }
+      })
+
+      return response
+
+    } catch (error: any) {
+
+      console.error("[error]:", JSON.stringify(error, null, 2))
+
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: error.message || "unknown error"
+      })
+
+    }
   })
   )
 })
