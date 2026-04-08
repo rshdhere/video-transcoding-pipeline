1b56e75099ba7bcc381c633955f4afe9d0103182
raashed <mohammedraashed10@gmail.com>
2026-04-08 07:57:03 +0530

    refactor(main): added /main fn as an express binary

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
-import express from "express"
-import { router } from "./trpc.js";
-import { userRouter } from "./routes/v1/user.js";
-import * as trpcExpress from "@trpc/server/adapters/express"
-import { createContext } from "./trpc/context.js";
-import { toNodeHandler } from "better-auth/node";
-import { auth } from "./auth/auth.js";
+import { app } from "./router.js";
+import { PORT } from "./config/config.js";
+import { queueWarmup } from "./queue/warmup.js";
+import { ensureDatabaseConnection } from "./drizzle/src/health.js";
 
-export const appRouter = router({
-  v1: router({
-    user: userRouter,
-  }),
-})
+export const main = async () => {
+  await ensureDatabaseConnection()
 
-export type AppRouter = typeof appRouter;
+  app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
+  });
 
-export const app = express();
-
-app.use("/trpc", trpcExpress.createExpressMiddleware({
-  router: appRouter,
-  createContext
-}))
-
-app.use("/api/auth/", toNodeHandler(auth));
+  await queueWarmup()
+}
