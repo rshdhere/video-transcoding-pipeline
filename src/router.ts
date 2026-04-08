285240c0672f832c7f7df4c48554d7977017fe29
raashed <mohammedraashed10@gmail.com>
2026-04-08 07:54:23 +0530

    feat(router): added /router for my trpc-express-adapter

---
+++ src/router.ts
@@ -0,0 +1,25 @@
+
+import express from "express"
+import { router } from "./trpc.js";
+import { userRouter } from "./routes/v1/user.js";
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { createContext } from "./trpc/context.js";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./auth/auth.js";
+
+export const appRouter = router({
+  v1: router({
+    user: userRouter,
+  }),
+})
+
+export type AppRouter = typeof appRouter;
+
+export const app = express();
+
+app.use("/trpc", trpcExpress.createExpressMiddleware({
+  router: appRouter,
+  createContext
+}))
+
+app.use("/api/auth/", toNodeHandler(auth));
