b6e4f37d0a893988f096c6cb0aaf9cf74270cb46
raashed <mohammedraashed10@gmail.com>
2026-04-08 15:26:20 +0530

    chore(queue-warmpup): removed warm-up script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { app } from "./router.js";
 import { PORT } from "./config/config.js";
-import { queueWarmup } from "./queue/warmup.js";
 import { ensureDatabaseConnection } from "./drizzle/src/health.js";
 
 export const main = async () => {
@@ -9,6 +8,4 @@ export const main = async () => {
   app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
   });
-
-  await queueWarmup()
 }
