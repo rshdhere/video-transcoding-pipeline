83ecd0343fe8c1d047ad1a57f9f7165bd1e449fa
raashed <mohammedraashed10@gmail.com>
2026-04-11 21:36:20 +0530

    chore(dashboard): added auth-routing to the dashboard and removed layout

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,3 +1,31 @@
-export default function Dashboard() {
+"use client"
 
+import { Button } from "@/components/ui/button"
+import { authClient } from "@/lib/auth-client"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
+export default function Dashboard() {
+  const router = useRouter()
+  const { data: session, isPending } = authClient.useSession()
+
+  useEffect(() => {
+    if (!isPending && !session) {
+      router.push("/login")
+    }
+  }, [session, isPending, router])
+
+  const handleLogout = async () => {
+    await authClient.signOut()
+    router.push("/login")
+  }
+
+  if (isPending || !session) return null
+
+  return (
+    <div>
+      <p>Hi, {session.user.name}!!</p>
+      <Button variant={'destructive'} onClick={handleLogout}>Logout</Button>
+    </div>
+  )
 }
